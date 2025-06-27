9269a6be454774f4042f712f3e590734c98d8d3b
secondmailaltt <secondmailaltt@gmail.com>
2025-06-28 00:34:54 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
-// // config/db.js
-// const mongoose = require("mongoose");
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log("MongoDB connected");
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-// };
-// module.exports = connectDB;
+// config/db.js
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
